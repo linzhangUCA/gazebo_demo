780ec72106672900efcf55033a8e7c1d2a26abb3
linzhanguca <lzhang12@uca.edu>
2022-02-13 15:53:42 -0600

    update launch according to nav2 tutorial

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*.launch.py")),
         ),
+        (
+            os.path.join("share", package_name, "worlds"),
+            glob(os.path.join("worlds", "*.sdf")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
